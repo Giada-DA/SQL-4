05a5f041131f90310371e5e07f23c5a37779cd4d
Giada Dall'Ara <giada.25700@gmail.com>
2023-03-20 23:32:05 +0100

    Esercizio 4 di sql

---
+++ Esercizio/src/Start.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class Start {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            conn= DriverManager.getConnection("jdbc:mysql://localhost:3307/newdb", "root", "root00");
+            statement = conn.createStatement();
+            String query = "ALTER TABLE newdb.students ADD country VARCHAR(30);";
+            statement.execute(query);
+            statement.execute("UPDATE newdb.students SET country = 'Gemania' WHERE student_id = 4;");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
