276186e6ac653c68e7ffb10183fe011732d1fbce
Giada Dall'Ara <giada.25700@gmail.com>
2023-04-04 11:18:57 +0200

    esercizio 4 di sql

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -11,6 +11,8 @@ public class Start {
             String query = "ALTER TABLE newdb.students ADD country VARCHAR(30);";
             statement.execute(query);
             statement.execute("UPDATE newdb.students SET country = 'Gemania' WHERE student_id = 4;");
+            statement.execute("UPDATE newdb.students SET country = 'Gemania' WHERE student_id = 1;");
+            statement.execute("UPDATE newdb.students SET country = 'Italia' WHERE student_id = 2 OR student_id = 3;");
         }catch (SQLException e){
             e.printStackTrace();
         }
